e0c9681fda0799ab966412f03151885d40cc09cc
AaronHeise <a.m.heise@gmail.com>
2016-11-05 12:46:29 -0700

    [updates] Developing strategy for OO approach

--- calculator.js
+++ calculator.js
@@ -14,8 +14,17 @@ split cleaned-up input into an array (don't worry about negatives to start)
 Add Methods to class:
 
 evaluateExpression(expression)
+  create Order of Operations tiers. Tier 1: multiplication & division. Tier 2: addition & subtraction
+  Iterate through the pairs
+    while either operator is still present in the expression, evaluate with priority from left to right
+    locate highest priority operator
+    split expression at operator location
+    take last element from left array and first element from right array
+    Feed these operands into doMath() with the appropriate operator
 
-orderOperations(expression, )
+assignOrderOperations(expression, operatorPair)
+  determine if only one of the operators is present, if so, select other one;
+  if both present, assign operator as first one in expression
 
 doMath
 
@@ -27,6 +36,14 @@ function Calculator(userInput) {
   console.log(this.cleanedInput);
   this.expressionArray = this.cleanedInput.split(/([^\d])/);
   console.log(this.expressionArray);
+}
+
+Calculator.prototype.resolveExpression = function(expression) {
+
+}
+
+Calculator.prototype.assignOrderOperations = function(expression, operatorPair) {
+
 }
 
 Calculator.prototype.doMath = function(num1, num2, operator) {
